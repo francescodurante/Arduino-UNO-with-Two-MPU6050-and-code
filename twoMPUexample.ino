b852ca4fa82428c9c07e45551010ce77da7b8b13
francescodurante <130830980+francescodurante@users.noreply.github.com>
2023-05-03 19:19:15 +0200

    Add files via upload

---
+++ twoMPUexample.ino
@@ -0,0 +1,37 @@
+#include <MPU6050_tockn.h>
+#include <MPU6050_tockn69.h>
+#include <Wire.h>
+#include <Servo.h>
+
+MPU605069 mpu60502(Wire);
+MPU6050 mpu6050(Wire);
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+  mpu60502.begin();
+  mpu60502.calcGyroOffsets(true);
+  
+}
+
+void loop() {
+  mpu6050.update();
+  Serial.print("angleX : ");
+  Serial.print(mpu6050.getAngleX());
+  Serial.print("\tangleY : ");
+  Serial.print(mpu6050.getAngleY());
+  Serial.print("\tangleZ : ");
+  Serial.print(mpu6050.getAngleZ());
+
+  mpu60502.update();
+  Serial.print(" angleX : ");
+  Serial.print(mpu60502.getAngleX());
+  Serial.print("\tangleY : ");
+  Serial.print(mpu60502.getAngleY());
+  Serial.print("\tangleZ : ");
+  Serial.println(mpu60502.getAngleZ());
+
+
+}
